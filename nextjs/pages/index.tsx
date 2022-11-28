c29ca0d1c3b4681168ecd4b451a1f79afaa00eb5
hyMew <103243630+hyMew@users.noreply.github.com>
2022-11-28 17:25:54 +0900

    nextjs

---
+++ nextjs/pages/index.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import Layout from '../components/layout'
+import styles from '../styles/Home.module.css'
+ 
+export default function Home() {
+    return (
+        <Layout
+            title="ホームページタイトル"
+            description="ホームページ概要"
+        >
+            <p>ホームページコンテンツ</p>
+        </Layout>
+    )
+}
