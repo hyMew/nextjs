217360c751ea3c8c8c5e12b15bbffb427511c3c0
hyMew <103243630+hyMew@users.noreply.github.com>
2022-11-28 17:55:17 +0900

    randam

--- nextjs/pages/index.tsx
+++ nextjs/pages/index.tsx
@@ -1,16 +1,23 @@
 import Link from 'next/link'
 import Layout from '../components/layout'
 import styles from '../styles/Home.module.css'
+import { useState } from "react"
 
 export default function Home() {
+
+    const [imageUrl, setImageUrl] = useState (
+        "//picsum.photos/400/400"
+    );
+
     return (
         <Layout
             title="ホームページタイトル"
             description="ホームページ概要"
         >
             <p>ホームページコンテンツ</p>
+            <p>いろいろテスト中です。</p>
             <figure className={styles.sampleImage}>
-                <img src="//picsum.photos/400/300"></img>
+                <img src={imageUrl}></img>
             </figure>
         </Layout>
     )
